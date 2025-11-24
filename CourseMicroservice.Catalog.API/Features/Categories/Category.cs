15d69302dcaf533cb54642d97b1095f2a261a004
Osman Tonbul <osmantonbul2001@gmail.com>
2025-11-24 23:56:20 +0300

    Created entities for catalog microservice

---
+++ CourseMicroservice.Catalog.API/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using CourseMicroservice.Catalog.API.Features.Courses;
+using CourseMicroservice.Catalog.API.Repositories;
+
+namespace CourseMicroservice.Catalog.API.Features.Categories
+{
+	public class Category : BaseEntity
+	{
+		public string Name { get; set; } = default;
+		public List<Course>? Courses { get; set; }
+	}
+}
