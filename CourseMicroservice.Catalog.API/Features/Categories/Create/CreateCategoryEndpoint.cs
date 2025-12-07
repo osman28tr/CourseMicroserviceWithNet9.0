ff37b151564bb5806c4394d0c092e5b5f45f8a4e
Osman Tonbul <osmantonbul2001@gmail.com>
2025-12-07 17:44:52 +0300

    test

---
+++ CourseMicroservice.Catalog.API/Features/Categories/Create/CreateCategoryEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using CourseMicroservice.Shared.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseMicroservice.Catalog.API.Features.Categories.Create
+{
+	public static class CreateCategoryEndpoint
+	{
+		public static RouteGroupBuilder CreateCategoryGroupItemEndpoint(this RouteGroupBuilder routeGroupBuilder)
+		{
+			routeGroupBuilder.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) => await mediator.Send(command));
+			
+			return routeGroupBuilder;
+		}
+	}
+}
