4c008b690dd77273507ad400d8e604ba502bd79f
Osman Tonbul <osmantonbul2001@gmail.com>
2025-12-09 21:57:59 +0300

    Global using configuration was carried out

---
+++ CourseMicroservice.Catalog.API/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using AutoMapper;
+global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using CourseMicroservice.Shared;
+global using CourseMicroservice.Shared.Extensions;
+global using MassTransit;
+global using System.Net;
+global using FluentValidation;
+global using CourseMicroservice.Catalog.API.Features.Categories.Dtos;
+global using CourseMicroservice.Catalog.API.Repositories;
