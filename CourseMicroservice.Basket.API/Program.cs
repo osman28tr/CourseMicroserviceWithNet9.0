bff73dbd1b3f8e374762f2a1719b4ca9face5276
Osman Tonbul <osmantonbul2001@gmail.com>
2025-12-21 22:33:17 +0300

    .

---
+++ CourseMicroservice.Basket.API/Program.cs
@@ -0,0 +1,19 @@
+using CourseMicroservice.Basket.API;
+using CourseMicroservice.Shared.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddCommonServiceExt(typeof(BasketAssembly));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.MapOpenApi();
+}
+
+app.Run();
+
