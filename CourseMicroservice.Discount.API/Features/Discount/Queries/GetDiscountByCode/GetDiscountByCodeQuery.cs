b1cdac9195fe4d12227aae0c15e2dadbd9ddbc62
Osman Tonbul <osmantonbul2001@gmail.com>
2026-04-23 00:09:57 +0300

    GetDiscountByCode operation was carried out.

---
+++ CourseMicroservice.Discount.API/Features/Discount/Queries/GetDiscountByCode/GetDiscountByCodeQuery.cs
@@ -0,0 +1,7 @@
+﻿using CourseMicroservice.Discount.API.Dtos;
+using static CourseMicroservice.Shared.Responses.ServiceResponse;
+
+namespace CourseMicroservice.Discount.API.Features.Discount.Queries.GetDiscountByCode
+{
+	public record GetDiscountByCodeQuery(string Code) : IRequestByServiceResponse<DiscountDto>;
+}
---
+++ CourseMicroservice.Discount.API/Features/Discount/Queries/GetDiscountByCode/GetDiscountByCodeQueryEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using CourseMicroservice.Discount.API.Features.Discount.Commands.Create;
+using CourseMicroservice.Shared.Filters;
+
+namespace CourseMicroservice.Discount.API.Features.Discount.Queries.GetDiscountByCode
+{
+	public static class GetDiscountByCodeQueryEndpoint
+	{
+		public static RouteGroupBuilder GetDiscountByCodeGroupItemEndpoint(this RouteGroupBuilder routeGroupBuilder)
+		{
+			routeGroupBuilder.MapGet("/GetDiscountByCode/{code}",
+				async (string code, IMediator mediator) =>
+					(await mediator.Send(new GetDiscountByCodeQuery(code))).ToGenericResult())
+				.MapToApiVersion(1, 0)
+			.AddEndpointFilter<ValidationFilter<GetDiscountByCodeQueryValidator>>();
+			return routeGroupBuilder;
+		}
+	}
+}
