4e6849207ab1fc703121e5cf5d9c71ccf1b2982d
Osman Tonbul <osmantonbul2001@gmail.com>
2026-04-23 19:28:21 +0300

    Order microservice was created and interlayer relationships were structured

---
+++ CourseMicroservice.Order.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMicroservice.Order.Domain.Entities
+{
+	public class BaseEntity<TEntityId>
+	{
+		public TEntityId EntityId { get; set; } = default!;
+	}
+}
