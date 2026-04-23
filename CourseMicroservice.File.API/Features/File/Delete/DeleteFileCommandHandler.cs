e109c9c7e77cd9d5aa24d2f9f2f58dc4ae725327
Osman Tonbul <osmantonbul2001@gmail.com>
2026-04-23 12:09:45 +0300

    Delete file operation was carried out and upload file operation revised by file extension

---
+++ CourseMicroservice.File.API/Features/File/Delete/DeleteFileCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using CourseMicroservice.Shared.Responses;
+using MediatR;
+using Microsoft.Extensions.FileProviders;
+
+namespace CourseMicroservice.File.API.Features.File.Delete
+{
+	public class DeleteFileCommandHandler(IFileProvider fileProvider) : IRequestHandler<DeleteFileCommand, ServiceResponse>
+	{
+		public async Task<ServiceResponse> Handle(DeleteFileCommand request, CancellationToken cancellationToken)
+		{
+			var fileInfo = fileProvider.GetFileInfo(Path.Combine("files", request.FileName));
+			if (!fileInfo.Exists)
+			{
+				return ServiceResponse.ErrorAsNotFound(); 
+			}
+			System.IO.File.Delete(fileInfo.PhysicalPath!);
+			return ServiceResponse.SuccessAsNoContent();
+		}
+	}
+}
--- CourseMicroservice.File.API/Features/File/Upload/UploadFileCommandHandler.cs
+++ CourseMicroservice.File.API/Features/File/Upload/UploadFileCommandHandler.cs
@@ -14,25 +14,38 @@ namespace CourseMicroservice.File.API.Features.File.Upload
 				if (request.File.Length == 0)
 				{
 					return ServiceResponse<UploadFileCommandResponse>.Error("File is empty", "The uploaded file is empty.", HttpStatusCode.BadRequest);
-
 				}
+				var fileExtension = Path.GetExtension(request.File.FileName);
 
-				var newFileName = $"{Guid.NewGuid()}{Path.GetExtension(request.File.FileName)}";
+				var newFileName = $"{Guid.NewGuid()}{fileExtension}";
 
-				var filePath = Path.Combine(fileProvider.GetFileInfo("files").PhysicalPath!, newFileName);
+				var fileVirtualPath = GetFileVirtualPath(fileExtension);
+
+				var filePath = Path.Combine(fileProvider.GetFileInfo(fileVirtualPath).PhysicalPath!, newFileName);
 
 				using var stream = new FileStream(filePath, FileMode.Create);
 
 				await request.File.CopyToAsync(stream, cancellationToken);
 
-				var response = new UploadFileCommandResponse(newFileName, $"files/{newFileName}", request.File.FileName);
+				var response = new UploadFileCommandResponse(newFileName, $"{fileVirtualPath}/{newFileName}", request.File.FileName);
 
 				return ServiceResponse<UploadFileCommandResponse>.SuccessAsCreated(response, filePath);
 			}
-			catch (Exception ex)
+			catch
 			{
 				return ServiceResponse<UploadFileCommandResponse>.Error("File upload failed", "", HttpStatusCode.InternalServerError);
 			}
+		}
+		public string GetFileVirtualPath(string fileExtension)
+		{
+			return fileExtension switch
+			{
+				".doc" or ".docx" => "files/word",
+				".xls" or ".xlsx" => "files/excel",
+				".pdf" => "files/pdf",
+				".jpg" or ".jpeg" or ".png" or ".gif" => "files/image",
+				_ => "files/other"
+			};
 		}
 	}
 }
